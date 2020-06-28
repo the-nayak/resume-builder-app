c84a7b36ec6570a37f59d4d4463223729e623361
the-nayak <nayak54325@gmail.com>
2020-06-28 15:29:09 +0530

    project setup with tailwind & vue complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div class="text-5xl bg-blue-500">hello pradeep</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
